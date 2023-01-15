56a9dbc7935657c50dfddd7f729fbc94c271b776
Chong Yi Yin <yiyinchong@gmail.com>
2023-01-15 21:51:03 +0800

    first commit

---
+++ android/app/src/main/kotlin/com/example/test_role1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.test_role1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/volunteer.dart
@@ -0,0 +1,43 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+import 'login.dart';
+
+class Volunteer extends StatefulWidget {
+  const Volunteer({super.key});
+
+  @override
+  State<Volunteer> createState() => _VolunteerState();
+}
+
+class _VolunteerState extends State<Volunteer> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Volunteer"),
+        actions: [
+          IconButton(
+            onPressed: () {
+              logout(context);
+            },
+            icon: Icon(
+              Icons.logout,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Future<void> logout(BuildContext context) async {
+    CircularProgressIndicator();
+    await FirebaseAuth.instance.signOut();
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(
+        builder: (context) => LoginPage(),
+      ),
+    );
+  }
+}
